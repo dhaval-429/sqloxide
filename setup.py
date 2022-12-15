0ca95dac5c73ea5718bd635a13c3424378f8b709
dhaval <dhavalmendapara123@gmail.com>
2022-12-16 05:23:09 +0530

    adding manylinux aarch64

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-packages = ["sqloxide"]
+packages = ["./sqloxide"]
 
 package_data = {"": ["*"]}
 
@@ -9,7 +9,7 @@ setup_kwargs = {
     "name": "sqloxide",
     "version": "0.1.26",
     "description": "Python bindings for sqlparser-rs",
-    "long_description": open("readme.md").read(),
+    "long_description": open("./readme.md").read(),
     "long_description_content_type": "text/markdown",
     "author": "Will Eaton",
     "author_email": "me@wseaton.com",
