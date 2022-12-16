74b2b44fd983ae72869d6a1998bf606650399626
dhaval <dhavalmendapara123@gmail.com>
2022-12-16 05:30:24 +0530

    adding manylinux aarch64

--- build-wheels.sh
+++ build-wheels.sh
@@ -5,10 +5,11 @@ curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
 cd $GITHUB_WORKSPACE/io
+cd io
 
 for PYBIN in /opt/python/cp{37,38,39,310,311}*/bin; do
     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
-    "${PYBIN}/python" io/setup.py bdist_wheel
+    "${PYBIN}/python" setup.py bdist_wheel
 done
 
 for whl in dist/*.whl; do
