ab463a2290e9375c0c96a271659a95341466953e
dhaval <dhavalmendapara123@gmail.com>
2022-12-18 19:39:01 +0530

    adding manylinux aarch64

--- build-wheels.sh
+++ build-wheels.sh
@@ -15,3 +15,8 @@ done
 for whl in dist/*.whl; do
     auditwheel repair "$whl" -w dist/
 done
+
+for PYBIN in /opt/python/cp{37,38,39,310,311}*/bin; do
+    "${pybin}/pip" install ${PACKAGE_NAME} --no-index -f dist/
+    "${pybin}/pytest" tests/test_sqloxide.py
+done
